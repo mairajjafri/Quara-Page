a16da2ea9d7b9893a249493b7da272e9c476ac53
Mairaj Jafri <mairajjafri@users.noreply.github.com>
2025-08-28 16:10:33 +0530

    4th commit, Creating Ids (UUID)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,18 +13,22 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [{
+        id: uuidv4(),
         username: "Mairaj Jafri",
         content: "I love coding",
     },
     {
+        id: uuidv4(),
         username: "Akshat Chaudhary",
         content: "Studying in Pune",
     },
     {
+        id: uuidv4(),
         username: "Aryan Satle",
         content: "Learning Tamil Language",
     },
     {
+        id: uuidv4(),
         username: "Ameya Laad",
         content: "Eating Indori Poha",
     }
@@ -30,8 +36,29 @@ let posts = [{
 
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
-})
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, content });
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
+    res.render("show.ejs", { post });
+});
+
 
 app.listen(port, () => {
     console.log(`listening to port: ${port}`);
-})
+});
